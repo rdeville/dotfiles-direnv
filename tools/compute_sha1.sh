dd05a8d13bd1bcc2e9f181c7ca07a3a9c3419602
Romain Deville <code@romaindeville.fr>
2024-03-24 19:42:57 +0100

    🔨 Update tools script

--- tools/compute_sha1.sh
+++ tools/compute_sha1.sh
@@ -1,111 +1,66 @@
 #!/usr/bin/env bash
-# """Recursively compute SHA1 sum of a list of files and folders
-#
-# SYNOPSIS:
-#   ./compute_sha1.sh
-#
-# DESCRIPTION:
-#   The script will compute SHA1 sum of every required files for directory
-#   environment and store these SHA1 into the corresponding file in `.sha1`
-#   folder with the same architecture.
-#
+# """TODO
 # """
 
-# List of files and folder
+# shellcheck disable=SC2034
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P )"
+SCRIPTNAME="$(basename "$0")"
+
+init_logger(){
+  local log_file="${XDG_CACHE_HOME:-${HOME}/.cache}/snippets/_log.sh"
+  local last_download_file="/tmp/_log.time"
+  local delai=14400             # 4 hours
+  # shellcheck disable=SC2155
+  local curr_time=$(date +%s)
+  local time="$(( curr_time - $(cat "${last_download_file}" 2>/dev/null || echo "0") ))"
+
+  if ! [[ -f "${log_file}" ]] \
+    || { [[ -f "${log_file}" ]] && [[ "${time}" -gt "${delai}" ]]; }
+  then
+    if ping -q -c 1 framagit.org &> /dev/null
+    then
+      # shellcheck disable=SC1090
+      source <(curl -s https://framagit.org/-/snippets/7183/raw/main/_get_log.sh)
+      echo "${curr_time}" > "${last_download_file}"
+    else
+      echo -e "\033[1;33m[WARNING]\033[0;33m Unable to get last logger version, will use `echo`.\033[0m"
+      _log(){
+        echo "$@"
+      }
+    fi
+  else
+    # shellcheck disable=SC1090
+    source <(cat "${log_file}")
+  fi
+}
+
+main(){
+  # TODO Change below substitution if need
+  local DEBUG_LEVEL="${DEBUG_LEVEL:-INFO}"
+  init_logger
+
+  for i_node in "${NODES[@]}"
+  do
+    local file="${XDG_CONFIG_HOME:-${HOME}/.config}/direnv/${i_node}"
+    # shellcheck disable=SC2155
+    local file_name="$(basename "${file}" | sed -e "s/^\.//")"
+    # shellcheck disable=SC2155
+    local file_dir="$(dirname "${file}")"
+    local sha1_file="${XDG_CACHE_HOME:-${HOME}/.cache}/direnv${file_dir/${HOME}/}/${file_name}.sha1"
+    if [[ -f "${sha1_file}" ]]
+    then
+      _log "WARNING" "Deleting ${sha1_file}"
+      rm -f ${sha1_file}
+    fi
+    _log "INFO" "Regenerate sha1sum of ${i_node}"
+    sha1sum "${file}" > "${sha1_file}"
+  done
+}
+
 NODES=(
-  ".envrc"
   "direnvrc"
-  "lib"
-  "src"
-  "modules"
-  "templates"
 )
 
-DEBUG_LEVEL=${DIRENV_DEBUG_LEVEL:-INFO}
-source <(curl -s https://framagit.org/-/snippets/7183/raw/main/_log.sh)
+main "$@"
 
-compute_sha1()
-{
-  # """Main recursive method that compute SHA1 of files and folder
-  #
-  # Check if the nodes is a folder or a file.
-  # If it is a file, compute its SHA1 sum and store it in `.sha1` folder.
-  # If it is a folder, store the folder in a temporary array to be passed
-  # recursively to this method.
-  #
-  # Globals:
-  #   DIRENV_ROOT
-  #   DIRENV_SHA1
-  #
-  # Arguments:
-  #   $@: (optional) string, list of files and folders
-  #
-  # Output:
-  #   Log informations
-  #
-  # Returns:
-  #   None
-  #
-  # """
-
-  local tmp_nodes=()
-  local file_from
-
-  for i_node in "$@"
-  do
-    file_from="${direnv_dir}/${i_node}"
-    if [[ -f "${file_from}" ]] && ! [[ "${file_from}" =~ __pycache__ ]]
-    then
-      _log "INFO" "Computing sha1 of **${direnv_dir}/${i_node}**."
-      sha1sum "${direnv_dir}/${i_node}"  | sed -e "s|${direnv_dir}/||" >> "${sha1_file}"
-    elif [[ -d "${file_from}" ]]
-    then
-      for i_subnode in "${file_from}"/*
-      do
-        # Remove every occurrences of `${DIRENV_ROOT}`
-        i_subnode="${i_subnode//${direnv_dir}\//}"
-        tmp_nodes+=("${i_subnode}")
-      done
-    fi
-  done
-
-  if [[ -n "${tmp_nodes[*]}" ]]
-  then
-    compute_sha1 "${tmp_nodes[@]}"
-  fi
-}
-
-main()
-{
-  # """Ensure directory environment is activated an run SHA1 sum computation
-  #
-  # Globals:
-  #   DIRENV_ROOT
-  #
-  # Arguments:
-  #   None
-  #
-  # Output:
-  #   Error informations
-  #
-  # Returns:
-  #   1 if directory environment is not activated
-  #   0 if everything went right
-  #
-  # """
-  local direnv_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/direnv"
-  local sha1_file="${direnv_dir}/direnv.sha1"
-  if [[ -f "${sha1_file}" ]]
-  then
-    rm "${sha1_file}"
-  fi
-
-  compute_sha1 "${NODES[@]}"
-}
-
-main
-
-# ------------------------------------------------------------------------------
-# VIM MODELINE
-# vim: ft=bash: foldmethod=indent
-# ------------------------------------------------------------------------------
+# vim: ft=sh
