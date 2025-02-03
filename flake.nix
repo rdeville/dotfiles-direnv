b720b0b5fd894b574ac2c0fa1841b2da64b1cbdd
Romain Deville <code@romaindeville.fr>
2025-02-03 09:36:07 +0100

    ➖ Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# BEGIN DOTGIT-SYNC BLOCK MANAGED
 {
   description = ''
     Flake for DirenvRC
@@ -10,31 +9,19 @@
   inputs = {
     # Stable Nix Packages
     nixpkgs = {
-      url = "nixpkgs/nixos-24.05";
-      # url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
     # Flake Utils Lib
     utils = {
       url = "github:numtide/flake-utils";
     };
-    alejandra = {
-      url = "github:kamadorueda/alejandra";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_INPUT
-    #
-    # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_INPUT
   };
+
   outputs = inputs @ {self, ...}: let
     pkgsForSystem = system:
       import inputs.nixpkgs {
         inherit system;
       };
-    # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_CUSTOM_VARS
-    #
-    # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_CUSTOM_VARS
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
     forAllSystems = inputs.nixpkgs.lib.genAttrs allSystems;
 
     allSystems = [
@@ -50,23 +37,19 @@
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
     formatter = forAllSystems (
       system:
-        inputs.alejandra.defaultPackage.${system}
+        (pkgsForSystem system).alejandra
     );
+
     homeManagerModules = {
       direnvrc = import ./modules/home-manager.nix self;
     };
     homeManagerModule = self.homeManagerModules.direnvrc;
 
-    # BEGIN DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_OUTPUTS_CUSTOM
-    overlays.default = final: prev: {
-      direnvrc = final.callPackage ./package.nix {};
-    };
     packages = forAllSystems (system: rec {
       direnvrc = with import inputs.nixpkgs {inherit system;};
         callPackage ./package.nix {};
       default = direnvrc;
     });
-    # END DOTGIT-SYNC BLOCK EXCLUDED NIX_FLAKE_OUTPUTS_CUSTOM
   };
 }
 # END DOTGIT-SYNC BLOCK MANAGED
