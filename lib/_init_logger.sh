6e67e06921f7a8e19375b446104907d7102e4550
Romain Deville <code@romaindeville.fr>
2025-02-06 23:57:09 +0100

    🐛 Fix init logger loading in direnvrc

--- lib/_init_logger.sh
+++ lib/_init_logger.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 _init_logger() {
-  # """Initiliaze bash logger script and allow access to _log() function"""
+  # """Initialize bash logger script and allow access to _log() function"""
   local log_file="${XDG_CACHE_HOME:-${HOME}/.cache}/snippets/_log.sh"
   local last_download_file="/tmp/_log.time"
   # 4 hours
@@ -11,7 +11,8 @@ _init_logger() {
   local time="$((curr_time - $(cat "${last_download_file}" 2>/dev/null || echo "0")))"
 
   if ! [[ -f "${log_file}" ]] ||
-    { [[ -f "${log_file}" ]] && [[ "${time}" -gt "${delai}" ]]; }; then
+    [[ $(wc -l ~/.cache/snippets/_log.sh | awk '{print $1}') -eq 0 ]] ||
+    { [[ -f "${log_file}" ]] && [[ "${time}" -gt "${delai}" ]]; } ; then
     if ping -q -c 1 -W 1 framagit.org &>/dev/null; then
       # shellcheck disable=SC1090
       source <(curl -s https://framagit.org/-/snippets/7183/raw/main/_get_log.sh)
